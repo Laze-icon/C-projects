13a8b2d8809ecbb2dee858e84b054cc7c8e9cd12
Laze_icon <eugenmboya376@gmail.com>
2025-03-04 22:08:16 +0300

    first commit

---
+++ 1D,2D,3D Matrices.cpp
@@ -0,0 +1,53 @@
+//C++ program creating 1D,2D and 3D matrices and their values
+//Eugen Mboya
+//BSCIT-05-0003/2024
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 1D matrix (array)
+    int oneD[5] = {1, 2, 3, 4, 5};
+    cout << "1D Matrix:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << oneD[i] << " ";
+    }
+    cout << endl;
+
+    // 2D matrix (array)
+    int twoD[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    cout << "2D Matrix:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << twoD[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // 3D matrix (array)
+    int threeD[2][2][2] = {
+        {
+            {1, 2},
+            {3, 4}
+        },
+        {
+            {5, 6},
+            {7, 8}
+        }
+    };
+    cout << "3D Matrix:" << endl;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                cout << threeD[i][j][k] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
